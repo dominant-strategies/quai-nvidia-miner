6984602211b663d96064c0787496a269d125296a
Djadih <13135116+Djadih@users.noreply.github.com>
2023-04-17 16:07:44 -0500

    Helpers to receive, decode, and write server messages to proxy

--- src/messages.h
+++ src/messages.h
@@ -1,5 +1,8 @@
-#ifndef ALEPHIUM_MESSAGE_H
-#define ALEPHIUM_MESSAGE_H
+#ifndef QUAI_MESSAGE_H
+#define QUAI_MESSAGE_H
+
+#define __STDC_FORMAT_MACROS
+
 
 #include <stdio.h>
 #include <stdint.h>
@@ -9,6 +12,9 @@
 #include <stdlib.h>
 
 #include "log.h"
+
+#define TARGET_LEN 32
+#define HEADER_LEN 32
 
 typedef struct blob_t {
     uint8_t *blob;
@@ -73,32 +79,19 @@ void hex_to_bytes(const char *hex_data, blob_t *buf)
     }
 }
 
+typedef struct header_msg_t {
+    char* header_msg;
+} header_msg_t;
+
 typedef struct job_t {
-    int from_group;
-    int to_group;
     blob_t header_blob;
-    blob_t txs_blob;
     blob_t target;
 } job_t;
 
 void free_job(job_t *job) {
     free_blob(&job->header_blob);
-    free_blob(&job->txs_blob);
     free_blob(&job->target);
     free(job);
-}
-
-typedef struct jobs_t {
-    job_t **jobs;
-    size_t len;
-} jobs_t;
-
-void free_jobs(jobs_t *jobs)
-{
-    for (size_t i = 0; i < jobs->len; i++) {
-        free_job(jobs->jobs[i]);
-    }
-    free(jobs->jobs);
 }
 
 typedef struct submit_result_t {
@@ -115,18 +108,32 @@ typedef enum server_message_kind {
 typedef struct server_message_t {
     server_message_kind kind;
     union {
-        jobs_t *jobs;
+        job_t *job;
         submit_result_t *submit_result;
     };
 } server_message_t;
+
+void free_server_message(server_message_t *message)
+{
+    switch (message->kind)
+    {
+    case JOBS:
+        free(message->job);
+        break;
+
+    case SUBMIT_RESULT:
+        free(message->submit_result);
+        break;
+    }
+
+    free(message);
+}
 
 void free_server_message_except_jobs(server_message_t *message)
 {
     switch (message->kind)
     {
     case JOBS:
-        free(message->jobs->jobs);
-        free(message->jobs);
         break;
 
     case SUBMIT_RESULT:
@@ -203,35 +210,10 @@ void extract_blob(uint8_t **bytes, blob_t *blob)
     ssize_t size = extract_size(bytes);
     blob->len = size;
     blob->blob = (uint8_t *)malloc(size * sizeof(uint8_t));
+    LOG("blob: %ld\n", blob->len);
     memcpy(blob->blob, *bytes, size);
     *bytes = *bytes + size;
 
-    // LOG("blob: %ld\n", blob->len);
-    // LOG("%s\n", bytes_to_hex(blob->blob, blob->len));
-}
-
-void extract_job(uint8_t **bytes, job_t *job)
-{
-    job->from_group = extract_size(bytes);
-    job->to_group = extract_size(bytes);
-    // LOG("group: %d, %d\n", job->from_group, job->to_group);
-    extract_blob(bytes, &job->header_blob);
-    extract_blob(bytes, &job->txs_blob);
-    extract_blob(bytes, &job->target);
-}
-
-void extract_jobs(uint8_t **bytes, jobs_t *jobs)
-{
-    ssize_t jobs_size = extract_size(bytes);
-
-    // LOG("jobs: %ld\n", jobs_size);
-
-    jobs->len = jobs_size;
-    jobs->jobs = (job_t **)malloc(jobs_size * sizeof(job_t*));
-    for(ssize_t i = 0; i < jobs_size; i++) {
-        jobs->jobs[i] = (job_t *)malloc(sizeof(job_t));
-        extract_job(bytes, (jobs->jobs[i]));
-    }
 }
 
 void extract_submit_result(uint8_t **bytes, submit_result_t *result)
@@ -243,53 +225,24 @@ void extract_submit_result(uint8_t **bytes, submit_result_t *result)
 
 server_message_t *decode_server_message(blob_t *blob)
 {
-    uint8_t *bytes = blob->blob;
-    ssize_t len = blob->len;
+    uint8_t *target = blob->blob;
+    uint8_t *header = blob->blob + TARGET_LEN;
 
-    if (len <= 4) {
-        return NULL; // not enough bytes for decoding
-    }
+    job_t* new_job = (job_t*) malloc(sizeof(job_t));
 
-    uint8_t *pos = bytes;
-    ssize_t message_size = extract_size(&pos);
-    assert(pos == bytes + 4);
+    new_job->target.len = TARGET_LEN;
+    new_job->target.blob = (uint8_t*) malloc(new_job->target.len * sizeof(uint8_t));
+    memcpy(new_job->target.blob, target, new_job->target.len);
 
-    ssize_t message_byte_size = message_size + 4;
-    if (len < message_byte_size) {
-        return NULL; // not enough bytes for decoding
-    }
+    new_job->header_blob.len = HEADER_LEN;
+    new_job->header_blob.blob = (uint8_t*) malloc(new_job->header_blob.len * sizeof(uint8_t));
+    memcpy(new_job->header_blob.blob, header, new_job->header_blob.len);
 
     server_message_t *server_message = (server_message_t *)malloc(sizeof(server_message_t));
-    switch (extract_byte(&pos))
-    {
-    case 0:
         server_message->kind = JOBS;
-        server_message->jobs = (jobs_t *)malloc(sizeof(jobs_t));
-        extract_jobs(&pos, server_message->jobs);
-
-        // LOG("%p, %p, %p\n", bytes, pos, bytes + len);
-        break;
-
-    case 1:
-        server_message->kind = SUBMIT_RESULT;
-        server_message->submit_result = (submit_result_t *)malloc(sizeof(submit_result_t));
-        extract_submit_result(&pos, server_message->submit_result);
-        break;
-
-    default:
-        LOGERR("Invalid server message kind\n");
-        exit(1);
-    }
-
-    assert(pos == (bytes + message_byte_size));
-    if (message_byte_size < len) {
-        blob->len = len - message_byte_size;
-        memmove(blob->blob, pos, blob->len);
-    } else {
-        blob->len = 0;
-    }
-
+        server_message->job = new_job;
+    
     return server_message;
 }
 
-#endif // ALEPHIUM_MESSAGE_H
+#endif // QUAI_MESSAGE_H
