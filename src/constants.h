9bc8b15ce5fe860930508bc094e775e76a195d51
Djadih <13135116+Djadih@users.noreply.github.com>
2023-06-08 15:19:56 -0500

    Default to one worker per GPU

--- src/constants.h
+++ src/constants.h
@@ -2,7 +2,7 @@
 #define QUAI_CONSTANTS_H
 
 #define max_gpu_num 1024
-#define parallel_mining_works_per_gpu 4
+#define parallel_mining_works_per_gpu 1
 #define max_worker_num (max_gpu_num * parallel_mining_works_per_gpu)
 #define mining_steps 5000
 
